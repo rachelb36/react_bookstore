eaae2ac7da5d08cbb9358ad23a9fb087eea7975b
rburgos <rachel@pixelstreetmedia.com>
2024-11-05 14:38:44 -0600

    pushed up typeDefs, Resolvers, queries and mutations and server.js, etc

--- server/server.js
+++ server/server.js
@@ -1,21 +1,29 @@
 const express = require('express');
-const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  await server.start();  // Make sure to await server.start()
+  server.applyMiddleware({ app });
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+startApolloServer();
